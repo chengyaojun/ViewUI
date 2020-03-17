e09994542e5c683a2944d356bbee9e5415ff565b
Ryder.Cheng <chengyaojun@outlook.com>
2020-03-17 14:25:29 +0800

    update(*): export date-picker/utils to md-extend/date.js

--- src/md-extend/components/table/v2/date-picker-cell-v2.vue
+++ src/md-extend/components/table/v2/date-picker-cell-v2.vue
@@ -1,48 +1,54 @@
 <template>
-  <DatePicker class="table-cell-date-picker" type="datetime" :dateFormat="dateFormat" placeholder="" :value="value" :transfer="transfer"  v-bind="$attrs" v-on="$listeners"></DatePicker>
+  <DatePicker  class="table-cell-date-picker" type="datetime" :format="dateFormat" placeholder="" v-model="value" :transfer="transfer"
+              v-bind="$attrs" v-on="$listeners"></DatePicker>
 </template>
 
 <script>
-    import dayjs from '../../../libs/dayjs';
-
-export default {
+  import { TYPE_VALUE_RESOLVER_MAP } from '../../../../components/date-picker/util';
+  export default {
     name: 'date-picker-cell',
-    props:{
-        date:String,
-        transfer:Boolean,
-        dateFormat:{
-            type:String,
-            default:'yyyy-MM-dd HH:mm:ss'
-        }
+    props: {
+      date: String,
+      transfer: Boolean,
+      dateFormat: {
+        type: String,
+        default: 'yyyy-MM-dd HH:mm:ss'
+      }
     },
-    watch:{
-        date:{
-            immediate:true,
-            handler(to) {
-                this.value = dayjs(to).format(this.dateFormat);
-            }
-        },
-        value:{
-            immediate:true,
-            handler(to) {
-                to&&this.$emit('on-change', to);
-            }
+    watch: {
+      date: {
+        immediate: true,
+        handler(to) {
+          let {
+            formatter,
+            parser,
+          } = TYPE_VALUE_RESOLVER_MAP.date;
+
+          this.value = formatter(parser(to, this.dateFormat), this.dateFormat)
         }
+      },
+      value: {
+        immediate: true,
+        handler(to) {
+          to && this.$emit('on-change', to);
+        }
+      }
     },
     data() {
-        return {
-            value: ''
-        };
+      return {
+        value: ''
+      };
     }
-};
+  };
 </script>
 
 <style lang="less">
-.table-cell-date-picker {
+  .table-cell-date-picker {
     border: 1px solid #d9d9d9;
+
     .ivu-date-picker-editor {
-        height: 46px;
-        line-height: 46px;
+      height: 46px;
+      line-height: 46px;
     }
-}
+  }
 </style>
---
+++ src/md-extend/libs/date.js
@@ -0,0 +1,19 @@
+import * as dateUtils from '../../components/date-picker/util';
+
+export const {
+    toDate,
+    clearHours,
+    isInRange,
+    formatDate,
+    parseDate,
+    getDayCountOfMonth,
+    getFirstDayOfMonth,
+    siblingMonth,
+    prevMonth,
+    nextMonth,
+    initTimeDate,
+    formatDateLabels,
+    DEFAULT_FORMATS,
+    TYPE_VALUE_RESOLVER_MAP
+} = dateUtils;
+export default dateUtils;
